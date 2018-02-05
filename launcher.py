a54bc0cb6f297fc3220d183f2da72f5226dd7486
ReinaSakuraba <juniorterra7@gmail.com>
2018-02-05 18:43:03 +0000

    Create db tables on startup

--- launcher.py
+++ launcher.py
@@ -6,9 +6,57 @@ from bot import Bot
 import config
 
 
+async def create_db(pool):
+    query = """
+            CREATE TABLE IF NOT EXISTS commands (
+                guild_id BIGINT,
+                channel_id BIGINT,
+                author_id BIGINT,
+                used TIMESTAMP DEFAULT NOW(),
+                prefix TEXT,
+                command TEXT
+            );
+
+            CREATE INDEX IF NOT EXISTS commands_used_idx ON commands(used);
+
+            CREATE TABLE IF NOT EXISTS plonks (
+                id SERIAL,
+                author_id BIGINT,
+                guild_id BIGINT
+            );
+
+            CREATE TABLE IF NOT EXISTS reminders (
+                id SERIAL,
+                message TEXT,
+                author_id BIGINT,
+                channel_id BIGINT,
+                created TIMESTAMP(0),
+                expires TIMESTAMP(0)
+            );
+
+            CREATE TABLE IF NOT EXISTS tags (
+                id SERIAL,
+                name TEXT,
+                location_id BIGINT,
+                owner_id BIGINT,
+                context TEXT,
+                uses INT DEFAULT 0,
+                created_at TIMESTAMP DEFAULT NOW(),
+                PRIMARY KEY(name, location_id)
+            );
+
+            CREATE UNIQUE INDEX IF NOT EXISTS tags_unique_idx ON tags(LOWER(name), location_id);
+            CREATE INDEX IF NOT EXISTS tags_name_idx ON tags(LOWER(name));
+            CREATE INDEX IF NOT EXISTS tags_location_idx ON tags(location_id);
+            CREATE INDEX IF NOT EXISTS tags_trgm_idx ON tags USING GIN (name gin_trgm_ops);
+            """
+    await pool.execute(query)
+
+
 def main():
     loop = asyncio.get_event_loop()
     pool = loop.run_until_complete(asyncpg.create_pool(config.dsn, command_timeout=60))
+    loop.run_until_complete(create_db(pool))
     bot = Bot(pool=pool, loop=loop)
     bot.run(config.token)
 
