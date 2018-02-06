9ac1844a81e28b9e9f15485fe8a75e5a0f9d5818
ReinaSakuraba <juniorterra7@gmail.com>
2018-02-06 23:14:29 +0000

    Add template config file

---
+++ config_template.py
@@ -0,0 +1,7 @@
+dsn = 'postgresql://poppi:yourpswd@localhost/poppi' # Your PSQL Database Not changed if following the README
+token = '' # Your bot's token
+description = 'A bot made by Reina#0277.' # The bot's description
+game = '@Poppi help' # The game status to be set
+prefix = '&' # The default command prefix
+feedback_channel = 123456789123456 # The channel to send feedback and error logs to
+youtube_key = '' # The YouTube API key to be used for YouTube commands
