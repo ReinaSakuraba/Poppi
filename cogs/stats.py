069ac8ae742c7f93b23469026a63945a12f514d7
ReinaSakuraba <juniorterra7@gmail.com>
2018-02-18 19:07:34 +0000

    Add global stats command

--- cogs/stats.py
+++ cogs/stats.py
@@ -48,6 +48,47 @@ class Stats:
 
         await ctx.send(embed=embed)
 
+    @stats.command(name='global')
+    async def stats_global(self, ctx, *, member: discord.Member = None):
+        member = member or ctx.author
+
+        embed = discord.Embed(title='Global Command Stats', color=member.color)
+        embed.set_author(name=member, icon_url=member.avatar_url)
+
+        query = 'SELECT COUNT(*) FROM commands WHERE author_id=$1;'
+        count = await ctx.pool.fetchval(query, member.id)
+
+        embed.description = f'{count} commands used'
+
+        query = """
+                SELECT command, COUNT(*) AS uses
+                FROM commands
+                WHERE author_id=$1
+                GROUP BY command
+                ORDER BY uses DESC
+                LIMIT 5;
+                """
+        records = await ctx.pool.fetch(query, member.id)
+        value = '\n'.join(f'{index}: {command} ({uses} uses)' for (index, (command, uses)) in enumerate(records, 1))
+
+        embed.add_field(name='Most Used Commands', value=value or 'No Commands', inline=False)
+
+        query = """
+                SELECT command, COUNT(*) AS uses
+                FROM commands
+                WHERE author_id=$1
+                and used > CURRENT_DATE
+                GROUP BY command
+                ORDER BY uses DESC
+                LIMIT 5;
+                """
+        records = await ctx.pool.fetch(query, member.id)
+        value = '\n'.join(f'{index}: {command} ({uses} uses)' for (index, (command, uses)) in enumerate(records, 1))
+
+        embed.add_field(name='Most Used Commands Today', value=value or 'No Commands', inline=False)
+
+        await ctx.send(embed=embed)
+
     @stats.command(name='server')
     @commands.guild_only()
     async def stats_server(self, ctx):
