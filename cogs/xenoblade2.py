bf2e9bda0aece05ef2b5d0c9e55a201257b383d1
ReinaSakuraba <juniorterra7@gmail.com>
2018-05-12 23:31:18 +0000

    Type hint xenoblade2 cog

--- cogs/xenoblade2.py
+++ cogs/xenoblade2.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 
+from bot import Bot
 import utils
 
 
 class Xenoblade2:
     @utils.group(invoke_without_command=True)
-    async def xc2art(self, ctx, *, name: str.lower):
+    async def xc2art(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 art."""
 
         query = """
@@ -32,7 +33,7 @@ class Xenoblade2:
                 WHERE LOWER(name)=$1;
                 """
 
-        record = await ctx.pool.fetchrow(query, name)
+        record = await ctx.pool.fetchrow(query, name.lower())
 
         if record is None:
             return await self.show_possibilities(ctx, 'arts', name)
@@ -60,25 +61,25 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2art.command(name='all')
-    async def xc2art_all(self, ctx):
+    async def xc2art_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 arts."""
 
         await self.all_entries(ctx, 'arts')
 
     @xc2art.command(name='search')
-    async def xc2art_search(self, ctx, *, name: str):
+    async def xc2art_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 art."""
 
         await self.search_entries(ctx, 'arts', name)
 
     @xc2art.error
     @xc2art_search.error
-    async def xc2art_error(self, ctx, error):
+    async def xc2art_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing art name.')
 
     @utils.group(invoke_without_command=True)
-    async def xc2special(self, ctx, *, name: str.lower):
+    async def xc2special(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 special."""
 
         query = """
@@ -101,7 +102,7 @@ class Xenoblade2:
                 WHERE LOWER(name)=$1;
                 """
 
-        record = await ctx.pool.fetchrow(query, name)
+        record = await ctx.pool.fetchrow(query, name.lower())
 
         if record is None:
             return await self.show_possibilities(ctx, 'specials', name)
@@ -126,25 +127,25 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2special.command(name='all')
-    async def xc2special_all(self, ctx):
+    async def xc2special_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 specials."""
 
         await self.all_entries(ctx, 'specials')
 
     @xc2special.command(name='search')
-    async def xc2special_search(self, ctx, *, name: str):
+    async def xc2special_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 special."""
 
         await self.search_entries(ctx, 'specials', name)
 
     @xc2special.error
     @xc2special_search.error
-    async def xc2special_error(self, ctx, error):
+    async def xc2special_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing special name.')
 
     @utils.group(invoke_without_command=True)
-    async def xc2skill(self, ctx, *, name: str.lower):
+    async def xc2skill(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 skill."""
 
         query = """
@@ -158,7 +159,7 @@ class Xenoblade2:
                 WHERE LOWER(name)=$1;
                 """
 
-        record = await ctx.pool.fetchrow(query, name)
+        record = await ctx.pool.fetchrow(query, name.lower())
 
         if record is None:
             return await self.show_possibilities(ctx, 'skills', name)
@@ -173,25 +174,25 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2skill.command(name='all')
-    async def xc2skill_all(self, ctx):
+    async def xc2skill_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 skills."""
 
         await self.all_entries(ctx, 'skills')
 
     @xc2skill.command(name='search')
-    async def xc2skill_search(self, ctx, *, name: str):
+    async def xc2skill_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 skill."""
 
         await self.search_entries(ctx, 'skills', name)
 
     @xc2skill.error
     @xc2skill_search.error
-    async def xc2skill_error(self, ctx, error):
+    async def xc2skill_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing skill name.')
 
     @utils.group(invoke_without_command=True)
-    async def xc2weapon(self, ctx, *, name: str.lower):
+    async def xc2weapon(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 weapon."""
 
         query = """
@@ -211,7 +212,7 @@ class Xenoblade2:
                 WHERE LOWER(name)=$1;
                 """
 
-        record = await ctx.pool.fetchrow(query, name)
+        record = await ctx.pool.fetchrow(query, name.lower())
 
         if record is None:
             return await self.show_possibilities(ctx, 'weapons', name)
@@ -232,25 +233,25 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2weapon.command(name='all')
-    async def xc2weapon_all(self, ctx):
+    async def xc2weapon_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 weapons."""
 
         await self.all_entries(ctx, 'weapons')
 
     @xc2weapon.command(name='search')
-    async def xc2weapon_search(self, ctx, *, name: str):
+    async def xc2weapon_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 weapon."""
 
         await self.search_entries(ctx, 'weapons', name)
 
     @xc2weapon.error
     @xc2weapon_search.error
-    async def xc2weapon_error(self, ctx, error):
+    async def xc2weapon_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing weapon name.')
 
     @utils.group(invoke_without_command=True)
-    async def xc2chip(self, ctx, *, name: str.lower):
+    async def xc2chip(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 core chip."""
 
         query = """
@@ -270,7 +271,7 @@ class Xenoblade2:
                 GROUP BY name;
                 """
 
-        record = await ctx.pool.fetchrow(query, name.replace(' chip', ''))
+        record = await ctx.pool.fetchrow(query, name.lower().replace(' chip', ''))
 
         if record is None:
             return await self.show_possibilities(ctx, 'chips', name)
@@ -287,25 +288,25 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2chip.command(name='all')
-    async def xc2chip_all(self, ctx):
+    async def xc2chip_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 core chips."""
 
         await self.all_entries(ctx, 'chips')
 
     @xc2chip.command(name='search')
-    async def xc2chip_search(self, ctx, *, name: str):
+    async def xc2chip_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 core chip."""
 
         await self.search_entries(ctx, 'chips', name)
 
     @xc2chip.error
     @xc2chip_search.error
-    async def xc2chip_error(self, ctx, error):
+    async def xc2chip_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing core chip name.')
 
     @utils.group(invoke_without_command=True)
-    async def xc2core(self, ctx, *, name: str.lower):
+    async def xc2core(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 aux core."""
 
         query = """
@@ -326,7 +327,7 @@ class Xenoblade2:
                 GROUP BY name
                 """
 
-        record = await ctx.pool.fetchrow(query, name)
+        record = await ctx.pool.fetchrow(query, name.lower())
 
         if record is None:
             return await self.show_possibilities(ctx, 'cores', name)
@@ -341,25 +342,25 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2core.command(name='all')
-    async def xc2core_all(self, ctx):
+    async def xc2core_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 aux cores."""
 
         await self.all_entries(ctx, 'cores')
 
     @xc2core.command(name='search')
-    async def xc2core_search(self, ctx, *, name: str):
+    async def xc2core_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 aux core."""
 
         await self.search_entries(ctx, 'cores', name)
 
     @xc2core.error
     @xc2core_search.error
-    async def xc2core_error(self, ctx, error):
+    async def xc2core_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing aux core name.')
 
     @utils.group(invoke_without_command=True)
-    async def xc2class(self, ctx, *, name: str.lower):
+    async def xc2class(self, ctx: utils.Context, *, name: str):
         """Gives information for a Xenoblade Chronicles 2 class."""
 
         query = """
@@ -380,7 +381,7 @@ class Xenoblade2:
                 GROUP BY name;
                 """
 
-        record = await ctx.pool.fetchrow(query, name)
+        record = await ctx.pool.fetchrow(query, name.lower())
 
         if record is None:
             return await self.show_possibilities(ctx, 'classes', name)
@@ -400,24 +401,24 @@ class Xenoblade2:
         await ctx.send(embed=embed)
 
     @xc2class.command(name='all')
-    async def xc2class_all(self, ctx):
+    async def xc2class_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles 2 classes."""
 
         await self.all_entries(ctx, 'classes')
 
     @xc2class.command(name='search')
-    async def xc2class_search(self, ctx, *, name: str):
+    async def xc2class_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles 2 class."""
 
         await self.search_entries(ctx, 'classes', name)
 
     @xc2class.error
     @xc2class_search.error
-    async def xc2class_error(self, ctx, error):
+    async def xc2class_error(self, ctx: utils.Context, error: Exception):
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send('Missing class name.')
 
-    async def show_possibilities(self, ctx, table_name, name):
+    async def show_possibilities(self, ctx: utils.Context, table_name: str, name: str):
         query = f"""
                 SELECT
                     STRING_AGG(name, E'\n' ORDER BY SIMILARITY(name, $1) DESC)
@@ -435,7 +436,7 @@ class Xenoblade2:
 
         await ctx.send(f'{type_name} not found. Did you mean...\n{possibilities}')
 
-    async def search_entries(self, ctx, table_name, name):
+    async def search_entries(self, ctx: utils.Context, table_name: str, name: str):
         query = f"""
                 SELECT ARRAY(
                     SELECT name
@@ -459,7 +460,7 @@ class Xenoblade2:
         except Exception as e:
             await ctx.send(e)
 
-    async def all_entries(self, ctx, table_name):
+    async def all_entries(self, ctx: utils.Context, table_name: str):
         query = f"""
                 SELECT ARRAY(
                     SELECT name
@@ -477,5 +478,5 @@ class Xenoblade2:
             await ctx.send(e)
 
 
-def setup(bot):
+def setup(bot: Bot):
     bot.add_cog(Xenoblade2())
