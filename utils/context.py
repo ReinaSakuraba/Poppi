84aa6ccb9d6ed71b64e263f29ea0b814a5b5af37
ReinaSakuraba <juniorterra7@gmail.com>
2018-09-11 00:18:47 +0000

    Add player property to context

--- utils/context.py
+++ utils/context.py
@@ -9,3 +9,7 @@ class Context(commands.Context):
     @property
     def pool(self):
         return self.bot.pool
+
+    @property
+    def player(self):
+        return self.bot.lavalink.players.get(self.guild.id) if self.guild else None
