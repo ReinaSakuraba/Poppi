5b8e0f1028a665131d9aab75bd0f5db110225884
ReinaSakuraba <juniorterra7@gmail.com>
2018-09-30 15:37:27 +0000

    Move search and all entries to utils

--- utils/misc.py
+++ utils/misc.py
@@ -1,8 +1,11 @@
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
+from .paginator import EmbedPaginator
+from .context import Context
 
-__all__ = ('run_subprocess', 'run_in_executor', 'CaseInsensitiveDict')
+
+__all__ = ('run_subprocess', 'run_in_executor', 'CaseInsensitiveDict', 'search_entries', 'all_entries')
 
 
 async def run_subprocess(cmd):
@@ -41,3 +44,59 @@ class CaseInsensitiveDict(dict):
 
     def __setitem__(self, k, v):
         super().__setitem__(k.lower(), v)
+
+
+async def search_entries(ctx: Context, schema: str, name: str, *table_names: str, type_name: str):
+    base_query = """
+                 SELECT name, SIMILARITY(name, $1)
+                 FROM {0}.{1}
+                 WHERE name % $1
+                 """
+
+    formatted_query = "UNION".join(base_query.format(schema, table_name) for table_name in table_names)
+
+    query = f"""
+            SELECT DISTINCT name, similarity
+            FROM (
+                {formatted_query}
+            ) AS a
+            ORDER BY similarity DESC;
+            """
+
+    results = [f'{index}: {name}' for index, (name, _) in enumerate(await ctx.pool.fetch(query, name), 1)]
+
+    if not results:
+        return await ctx.send(f'{type_name} not found.')
+
+    try:
+        paginator = EmbedPaginator(ctx, entries=results, per_page=15)
+        paginator.embed.colour = 0x738bd7
+        await paginator.paginate()
+    except Exception as e:
+        await ctx.send(e)
+
+
+async def all_entries(ctx: Context, schema: str, *table_names: str):
+    base_query = """
+                 SELECT name
+                 FROM {0}.{1}
+                 """
+
+    formatted_query = "UNION".join(base_query.format(schema, table_name) for table_name in table_names)
+
+    query = f"""
+            SELECT DISTINCT name
+            FROM (
+                {formatted_query}
+            ) AS a
+            ORDER BY name;
+            """
+
+    results = [f'{index}: {name}' for index, (name,) in enumerate(await ctx.pool.fetch(query), 1)]
+
+    try:
+        paginator = EmbedPaginator(ctx, entries=results, per_page=15)
+        paginator.embed.colour = 0x738bd7
+        await paginator.paginate()
+    except Exception as e:
+        await ctx.send(e)
