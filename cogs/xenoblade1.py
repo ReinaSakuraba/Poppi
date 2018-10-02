0d8fa863ca48a3e8927d5d00bc8705caa1aa3969
ReinaSakuraba <juniorterra7@gmail.com>
2018-10-02 12:47:07 +0000

    Use utils function for xeno 1 cog

--- cogs/xenoblade1.py
+++ cogs/xenoblade1.py
@@ -40,67 +40,13 @@ class Xenoblade:
     async def xc1skill_all(self, ctx: utils.Context):
         """Lists all Xenoblade Chronicles skills."""
 
-        await self.all_entries(ctx, 'skills')
+        await utils.all_entries(ctx, 'xeno1', 'skills')
 
     @xc1skill.command(name='search')
     async def xc1skill_search(self, ctx: utils.Context, *, name: str):
         """Searches for a Xenoblade Chronicles skill."""
 
-        await self.search_entries(ctx, name, 'skills', type_name='Skill')
-
-    async def search_entries(self, ctx: utils.Context, name: str, *table_names: str, type_name: str):
-        base_query = """
-                     SELECT name, SIMILARITY(name, $1)
-                     FROM xeno1.{0}
-                     WHERE name % $1
-                     """
-
-        formatted_query = "UNION".join(base_query.format(table_name) for table_name in table_names)
-
-        query = f"""
-                SELECT DISTINCT name, similarity
-                FROM (
-                    {formatted_query}
-                ) AS a
-                ORDER BY similarity DESC;
-                """
-
-        results = [f'{index}: {name}' for index, (name, _) in enumerate(await ctx.pool.fetch(query, name), 1)]
-
-        if not results:
-            return await ctx.send(f'{type_name} not found.')
-
-        try:
-            paginator = utils.EmbedPaginator(ctx, entries=results, per_page=15)
-            paginator.embed.colour = 0x738bd7
-            await paginator.paginate()
-        except Exception as e:
-            await ctx.send(e)
-
-    async def all_entries(self, ctx: utils.Context, *table_names: str):
-        base_query = """
-                     SELECT name
-                     FROM xeno1.{0}
-                     """
-
-        formatted_query = "UNION".join(base_query.format(table_name) for table_name in table_names)
-
-        query = f"""
-                SELECT DISTINCT name
-                FROM (
-                    {formatted_query}
-                ) AS a
-                ORDER BY name;
-                """
-
-        results = [f'{index}: {name}' for index, (name,) in enumerate(await ctx.pool.fetch(query), 1)]
-
-        try:
-            paginator = utils.EmbedPaginator(ctx, entries=results, per_page=15)
-            paginator.embed.colour = 0x738bd7
-            await paginator.paginate()
-        except Exception as e:
-            await ctx.send(e)
+        await utils.search_entries(ctx, 'xeno1', name, 'skills', type_name='Skill')
 
 
 def setup(bot):
