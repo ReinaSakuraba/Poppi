5bc92a795ef68619eaa23be945b45fde0e26cd94
ReinaSakuraba <juniorterra7@gmail.com>
2018-11-01 10:30:39 +0000

    Typehint context.py

--- utils/context.py
+++ utils/context.py
@@ -1,15 +1,19 @@
+import aiohttp
+import asyncpg
 from discord.ext import commands
+
+from cogs.music import Player
 
 
 class Context(commands.Context):
     @property
-    def session(self):
+    def session(self) -> aiohttp.ClientSession:
         return self.bot.session
 
     @property
-    def pool(self):
+    def pool(self) -> asyncpg.pool.Pool:
         return self.bot.pool
 
     @property
-    def player(self):
+    def player(self) -> Player:
         return self.bot.lavalink.players.get(self.guild.id) if self.guild else None
